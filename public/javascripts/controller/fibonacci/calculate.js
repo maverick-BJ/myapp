9b2fc1d25748ea2977d782081c999225384be89b
maverick-BJ <maverick02012@gmail.com>
2016-04-22 00:55:30 +0800

    斐波那契计算
    完成斐波那契的计算，前台展示与后台计算

--- public/javascripts/controller/fibonacci/calculate.js
+++ public/javascripts/controller/fibonacci/calculate.js
@@ -16,5 +16,54 @@ angular.module('myapp', [])
 
 
 function FibonacciCalculateCtrl ($scope) {
-	$scope.testvar = "JUSTTEST"
+	// $scope.testvar = "JUSTTEST"
+
+	//计算斐波那契额
+	//返回数值 和计算时间
+	$scope.calculate = function() {
+		console.log($scope.calculateTimes);
+		$.ajax({
+			url: "calculate_fibonaci",
+			data: {times:$scope.calculateTimes},
+			type: "POST",
+			success: function(ret) {
+				// console.log("返回结果",ret);
+				if(ret.success) {
+					//解决异步页面不渲染
+					$scope.$apply(function() {
+						$scope.calculateResult = ret.result;
+						$scope.calculateSuccess = true;
+						$scope.calculateError = false;
+					})
+					
+				}
+				else {
+					$scope.$apply(function() {
+						$scope.calculateError = true;
+						$scope.calculateSuccess = false;
+					})
+				}
+			},
+			error: function() {
+				$scope.$apply(function() {
+					$scope.calculateError = true;
+					$scope.calculateSuccess = false;
+				})
+			}
+
+		})
+
+	}//calculate
+
+	//初始化结果框
+	$scope.initResult = function() {
+		console.log("init");
+		$scope.calculateError = false;
+		$scope.calculateSuccess = false;
+	}
+	// $scope.$watch($scope.calculateTimes, function( ) {
+	// 	    console.log('$scope.name was updated!');
+	// 	    $scope.calculateError = false;
+	// 			$scope.calculateSuccess = false;
+	// });
 }
--- routes/fibonacci.js
+++ routes/fibonacci.js
@@ -1,16 +1,56 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+//简介页面
 router.get('/', function(req, res, next) {
-	console.log('fibonaci success!')
-	res.render('fibonacci/index', { title: 'Express' });
+	console.log('fibonaci success!');
+	res.render('fibonacci/index', { title: 'fibonacci 简介' });
 });
 
-/* GET home page. */
+//计算页面
 router.get('/calculate', function(req, res, next) {
-	console.log('fibonaci2 success!')
-	res.render('fibonacci/calculate', { title: 'Express' });
+	console.log('fibonaci2 success!');
+	res.render('fibonacci/calculate', { title: 'fibonacci 计算' });
 });
+
+//post 计算 返回计算时间
+router.post('/calculate_fibonaci', function(req, res, next) {
+	console.log('calculate_fibonaci',req.body.times+1);
+	var times = parseInt(req.body.times);
+	if(times > 0 && times < 199) {
+		var ret = Calculate(times);
+		res.send({success:true, result: ret.result});
+	}
+	else {
+		res.send({success:false});
+
+	}
+	// res.render('fibonacci/calculate', { title: 'Express' });
+});
+
+//计算
+Calculate = function(times) {
+	var result;
+	if(0 == times) {
+		result = 0;
+	}
+	if(1 == times) {
+		result = 1;
+	}
+	else {
+		var i = 0;
+		var j = 1;
+		var t = 0;
+		times = times - 1;
+
+		for(; times > 0; times--){
+			t = i + j;
+			i = j;
+			j = t;
+		}
+		result = t;
+	}
+	return {result:result};
+}
 
 module.exports = router;
